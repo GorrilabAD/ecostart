e6b6e39a6a44cd9961af9cc52e3ea7b6bfb23bc9
Gorrila <wrestbild57@gmail.com>
2018-09-16 20:51:18 +0300

    new changes

--- src/send.php
+++ src/send.php
@@ -1,23 +1,27 @@
 <?php
 $fio = $_POST['fio'];
 $number = $_POST['number'];
-$email = $_POST['email'];
+$email = $_POST['contact-email'];
+$message = $_POST['contact-message'];
 
 $fio = htmlspecialchars($fio);
 $number = htmlspecialchars($number);
 $email = htmlspecialchars($email);
+$message = htmlspecialchars($message);
 
 $fio = urldecode($fio);
 $number = urldecode($number);
 $email = urldecode($email);
+$message = urldecode($message);
 
 $fio = trim($fio);
 $number = trim($number);
 $email = trim($email);
+$message = trim($message);
 //echo $fio;
 //echo "<br>";
 //echo $email;
-if (mail("wrestbild57@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+if (mail("wrestbild57@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email,"Сообщение:".$message,"From: example2@mail.ru \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
